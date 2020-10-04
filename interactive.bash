3adbee197b0a062bbf273750b1745078894f7647
mymileniummi <70578568+mymileniummi@users.noreply.github.com>
2020-10-04 13:27:11 +0300

    Update interactive.bash

--- interactive.bash
+++ interactive.bash
@@ -15,6 +15,8 @@ function menu {
 	read code
 	case $code in
 		C)
+		if [ -e calc.bash ]
+		then 
 			. ./calc.bash
 			echo "Enter the operation"
 			read  arg1
@@ -23,25 +25,43 @@ function menu {
 			echo "Enter second number"
 			read  arg3
 			calc 4 $arg1 $arg2 $arg3
+		else
+		echo -e "\033[31mcalc.bash file is damaged function is not available\033[0m"
+		fi
 		;;
 		S)
+		if [ -e search.bash ]
+		then
 			. ./search.bash
 			echo "Enter directory and regular expression"
 			read arg1
 			read arg2
 			search 3 $arg1 $arg2
+		else
+		echo -e "\033[31msearch.bash file is damaged function is not available\033[0m"
+		fi
 		;;
 		R)
+		if [ -e reverse.bash ]
+		then 
 			. ./reverse.bash
 			echo "Enter first file name"
 			read arg1
 			echo "Enter second file name"
 			read arg2
-			reverse $arg1 $arg2
+			reverse $arg1 $arg2	
+		else
+		echo -e "\033[31mreverse.bash file is damaged function is not available\033[0m"
+		fi
 		;;
 		L)
+		if [ -e log.bash ]
+		then 
 			. ./log.bash
 			log
+		else
+		echo -e "\033[31mlog.bash file is damaged function is not available\033[0m"
+		fi
 		;;
 		STR)
 			echo "Enter a phrase"
@@ -49,14 +69,24 @@ function menu {
 			echo ${#arg1}
 		;;
 		E)
+		if [ -e exit.bash ]
+		then 
 			echo "Exit code:"
 			read arg1
 			. ./exit.bash
 			eXXit $arg1
+		else
+		echo -e "\033[31mexit.bash file is damaged function is not available\033[0m"
+		fi
 		;;
 		H)
+		if [ -e help.bash ]
+		then 
 			. ./help.bash
 			help
+		else
+		echo -e "\033[31mhelp.bash file is damaged function is not available\033[0m"
+		fi
 		;;
 		*)
 			echo -e  "\033[31mUnknown command\033[0m"
