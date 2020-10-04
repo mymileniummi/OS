b319a344d0a6e8b33c1d64fb77b9110da12105ee
mymileniummi <70578568+mymileniummi@users.noreply.github.com>
2020-10-04 18:39:28 +0300

    Create arg_count_check.bash

---
+++ arg_count_check.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+function arg_count {
+  if [ $1 -lt $2 ]
+  then
+    echo -e "\033[31mNot enough arguments \033[0m"
+     return 1
+  elif [ $1 -gt $2 ]
+  then 
+     ecko -e "\033[31mToo many arguments\033[0m"
+     return 1
+  else
+   return 0
+   fi
+}
