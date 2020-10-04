fc8403e83459eeb0d7c848fcc90667a312ee5133
mymileniummi <70578568+mymileniummi@users.noreply.github.com>
2020-10-04 18:47:16 +0300

    Update interactive.bash

--- interactive.bash
+++ interactive.bash
@@ -5,70 +5,63 @@ function menu {
  	   echo -e "
 ---===\033[36mMenu\033[0m===---
  enter:
-1) C for calc
-2) S for search
-3) R for reverse
-4) STR for strlen
-5) L for log
-6) E for exit
-7) H for help"
+1) c for calc
+2) s for search
+3) r for reverse
+4) str for strlen
+5) l for log
+6) e for exit
+7) h for help"
 	read code
 	case $code in
-		C)
+		c)
 		if [ -e calc.bash ]
 		then 
 			. ./calc.bash
-			echo "Enter the operation"
-			read  arg1
-			echo "Enter first number"
-			read  arg2
-			echo "Enter second number"
-			read  arg3
+			echo "Enter the operation, first, and second numbers"
+			read  arg1 arg2 arg3
 			calc 4 $arg1 $arg2 $arg3
 		else
 		echo -e "\033[31mcalc.bash file is damaged function is not available\033[0m"
 		fi
 		;;
-		S)
+		s)
 		if [ -e search.bash ]
 		then
 			. ./search.bash
 			echo "Enter directory and regular expression"
-			read arg1
-			read arg2
+			read arg1 arg2
 			search 3 $arg1 $arg2
 		else
 		echo -e "\033[31msearch.bash file is damaged function is not available\033[0m"
 		fi
 		;;
-		R)
+		r)
 		if [ -e reverse.bash ]
 		then 
 			. ./reverse.bash
-			echo "Enter first file name"
-			read arg1
-			echo "Enter second file name"
-			read arg2
-			reverse $arg1 $arg2	
+			echo "Enter first and second file name"
+			read arg1 arg2
+			reverse  3 $arg1 $arg2	
 		else
 		echo -e "\033[31mreverse.bash file is damaged function is not available\033[0m"
 		fi
 		;;
-		L)
+		l)
 		if [ -e log.bash ]
 		then 
 			. ./log.bash
-			log
+			log 1
 		else
 		echo -e "\033[31mlog.bash file is damaged function is not available\033[0m"
 		fi
 		;;
-		STR)
+		str)
 			echo "Enter a phrase"
 			read arg1
 			echo ${#arg1}
 		;;
-		E)
+		e)
 		if [ -e exit.bash ]
 		then 
 			echo "Exit code:"
@@ -79,7 +72,7 @@ function menu {
 		echo -e "\033[31mexit.bash file is damaged function is not available\033[0m"
 		fi
 		;;
-		H)
+		h)
 		if [ -e help.bash ]
 		then 
 			. ./help.bash
