253054e9c8e8a73840fe9ef6965874d13c861f1f
mymileniummi <70578568+mymileniummi@users.noreply.github.com>
2020-10-04 19:26:20 +0300

    Update log.bash

--- log.bash
+++ log.bash
@@ -4,9 +4,15 @@ function log {
       arg_count $1 1
       if [ $? -eq 0 ]
       then  
-     inf="\\(II\\)"
-     war="\\(WW\\)"
+            if [ ! -e /var/log/anaconda/X.log ]
+            then echo -e "\03331mLog file doesn't exist\033[0m"
+            elif [ ! -r /var/log/anaconda/X.log ]
+            then echo -e "Not enough rights for reading log file\033[0m"
+            else
+                  inf="\\(II\\)"
+                  war="\\(WW\\)"
 grep -E -h $war /var/log/anaconda/X.log | sed -E "s/$war/Warning: /"| sed -rn '/Warning:/s//\x1b[33m&\x1b[0m/p'
 grep -E -h $inf /var/log/anaconda/X.log | sed -E "s/$inf/Information: /" |sed -rn '/Information:/s//\x1b[36m&\x1b[0m/p'
-fi
+            fi      
+      fi
 }
