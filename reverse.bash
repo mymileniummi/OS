48e52356a028d1cbef31df2ea8ebe44c23135f53
user <user@localhost.localdomain>
2020-10-09 01:22:08 +0300

    Trying to fix reverse

--- reverse.bash
+++ reverse.bash
@@ -4,23 +4,28 @@ function reverse {
 	arg_count $1 3
 	if [ $? -eq 0 ]
 	then
-		if [ ! -e $2 ]
+		if [ $2 == $3 ]
 		then
-			echo -e "\033[31m File for reading doesnt exists \033[0m"
+		rev $2 >> $3
+		echo "Reversed successfully"
+		elif [ ! -e $2 ]
+		then
+			echo -e "\033[31m File for reading doesn't exist\033[0m"
 		elif [ $3 = "" ]
 		then
 			echo -e "\033[31m Can not create file with unknown name \033[0m"
 		elif [ ! -r $2 ]
 		then
 			echo -e "\033[31m Not enough rights for reading file \033[0m"
-		elif [ -e $3 -a ! -r $3 ]
+		elif [ -e $3 -a ! -w $3 ]
 		then
 		        echo -e "\033[31m Not enough rights for writing in file \033[0m"
+		elif [ -e $3 ] && [ ! -w $3 ] || [[ -d $3 ]] || ! touch "$3" 2> /dev/null;
+		then
+			echo -e "\033[31m Can't write in file \033[0m"
 		else
-			echo -n > temp.txt
-			cat $2 > temp.txt
-			rev temp.txt > $3
-			echo "Reversed succesfully"
+			rev $2>$3
+			echo "Reversed successfully"
 		fi
 	fi
 }
