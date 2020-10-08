bf419db3b0d32e8c2d6bfc8f97958a15ee569c96
user <user@localhost.localdomain>
2020-10-08 22:44:45 +0300

    Log fix

--- log.bash
+++ log.bash
@@ -3,16 +3,26 @@ function log {
       . ./arg_count_check.bash
       arg_count $1 1
       if [ $? -eq 0 ]
-      then  
+      then
             if [ ! -e /var/log/anaconda/X.log ]
             then echo -e "\03331mLog file doesn't exist\033[0m"
             elif [ ! -r /var/log/anaconda/X.log ]
             then echo -e "Not enough rights for reading log file\033[0m"
             else
-                  inf="\\(II\\)"
-                  war="\\(WW\\)"
-grep -E -h $war /var/log/anaconda/X.log | sed -E "s/$war/Warning: /"| sed -rn '/Warning:/s//\x1b[33m&\x1b[0m/p'
-grep -E -h $inf /var/log/anaconda/X.log | sed -E "s/$inf/Information: /" |sed -rn '/Information:/s//\x1b[36m&\x1b[0m/p'
-            fi      
+            while read line;
+	    do
+	    if [[ "$line" == *"(WW)"* && "$line" != *"(WW) warning, (EE) error, (NI) not implemented, (??) unknown."* ]]
+	    then
+		 echo -e ${line//"(WW)"/"\e[33mWarning: \e[0m"}
+            fi
+	    done < "/var/log/anaconda/X.log"
+	    while read line;
+	    do
+	    if [[ "$line" == *"(II)"* && "$line" != *"(++) from command line, (!!) notice, (II) informational,"* ]];
+	    then 
+		echo -e  ${line//"(II)"/"\e[36mInformation:\e[0m"}
+	    fi
+	    done < "/var/log/anaconda/X.log"
+            fi
       fi
 }
